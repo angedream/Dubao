f586e493a0277f33994a4a4f6773e1f36cfeff58
zilong <angeredream@126.com>
2026-04-15 15:56:59 +0800

    首次提交

--- app/src/main/java/com/zilong/dubao/HelloWorldActivity.java
+++ app/src/main/java/com/zilong/dubao/HelloWorldActivity.java
@@ -10,5 +10,6 @@ public class HelloWorldActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_world);
+        setTitle("hello zilong");
     }
 }
