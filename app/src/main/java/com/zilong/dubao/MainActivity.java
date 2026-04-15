e4e5f66d315a3bb0cf3c2550cb7f79495450656b
zilong <angeredream@126.com>
2026-04-15 14:19:19 +0800

    首次提交

---
+++ app/src/main/java/com/zilong/dubao/MainActivity.java
@@ -0,0 +1,15 @@
+package com.zilong.dubao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("hello zilong");
+    }
+}
