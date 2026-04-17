26e316d4c9b0a3c22103a2fabb34c77913b2ca83
zilong <angeredream@126.com>
2026-04-17 15:47:47 +0800

    ImageView

--- app/src/main/java/com/zilong/dubao/HelloWorldActivity.java
+++ app/src/main/java/com/zilong/dubao/HelloWorldActivity.java
@@ -1,10 +1,24 @@
 package com.zilong.dubao;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
 public class HelloWorldActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_world);
+        Button btn = findViewById(R.id.btn_submit);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 点击事件
+                ImageView imageView =findViewById(R.id.img);
+                imageView.setImageResource(R.drawable.dubao2);
+            }
+        });
+
     }
 }
