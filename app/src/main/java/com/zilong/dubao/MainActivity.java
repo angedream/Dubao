158ca882816be053aa7377c38d46211380945f3c
zilong <angeredream@126.com>
2026-04-20 13:34:15 +0800

    生命周期

--- app/src/main/java/com/zilong/dubao/MainActivity.java
+++ app/src/main/java/com/zilong/dubao/MainActivity.java
@@ -3,6 +3,7 @@ package com.zilong.dubao;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -13,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("MainActivity","onCreate被调用");
+
         Button btn = findViewById(R.id.btn_change);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,5 +25,39 @@ public class MainActivity extends AppCompatActivity {
                 imageView.setImageResource(R.drawable.dubao2);
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("MainActivity","onStart被调用");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("MainActivity","onResume被调用");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("MainActivity","onPause被调用");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("MainActivity","onStop被调用");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("MainActivity","onDestroy被调用");
+
     }
 }
