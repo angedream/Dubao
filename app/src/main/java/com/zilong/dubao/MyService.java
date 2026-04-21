9fcb34c2fab22a252e3518a6a0763f611330a75b
zilong <angeredream@126.com>
2026-04-21 16:04:59 +0800

    后台服务

---
+++ app/src/main/java/com/zilong/dubao/MyService.java
@@ -0,0 +1,40 @@
+package com.zilong.dubao;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.widget.Toast;
+
+public class MyService extends Service {
+    public MyService() {
+    }
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Toast.makeText(this,"后台服务onCreate",Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Toast.makeText(this,"后台服务onStartCommand",Toast.LENGTH_SHORT).show();
+        return super.onStartCommand(intent, flags, startId);
+
+    }
+
+    @Override
+    public void onDestroy() {
+
+        Toast.makeText(this,"后台服务onDestroy",Toast.LENGTH_SHORT).show();
+
+        super.onDestroy();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
