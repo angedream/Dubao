eca40fe218b0047fc857bebfd7080113a8179f14
zilong <angeredream@126.com>
2026-04-21 16:04:59 +0800

    后台服务

--- app/src/main/java/com/zilong/dubao/MainActivity.java
+++ app/src/main/java/com/zilong/dubao/MainActivity.java
@@ -6,22 +6,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button=findViewById(R.id.start);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, HelloWorldActivity.class);
-                Fish fish=new Fish("鲫鱼",24,9.9);
-                intent.putExtra("fish",fish);
-                startActivity(intent);
-
-            }
+        Button startbtn=findViewById(R.id.startbtn);
+        startbtn.setOnClickListener(v->{
+            Intent intent = new Intent(this, MyService.class);
+            startService(intent);
+        });
+        Button stopbtn=findViewById(R.id.stopbtn);
+        stopbtn.setOnClickListener(v->{
+            Intent intent = new Intent(this, MyService.class);
+            stopService(intent);
         });
     }
+
 }
