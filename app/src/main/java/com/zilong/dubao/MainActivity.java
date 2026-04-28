694bea8bb7c6653a5c7451c549ed832ed7e7a965
zilong <angeredream@126.com>
2026-04-28 16:36:54 +0800

    mqtt UUID

--- app/src/main/java/com/zilong/dubao/MainActivity.java
+++ app/src/main/java/com/zilong/dubao/MainActivity.java
@@ -1,29 +1,21 @@
 package com.zilong.dubao;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    MyMqttClient myMqttClient;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        myMqttClient=new MyMqttClient();
-        myMqttClient.connect();
         Button startbtn=findViewById(R.id.sendbtn);
         startbtn.setOnClickListener(v->{
-            String topic=((EditText)findViewById(R.id.topic)).getText().toString();
-            String content=((EditText)findViewById(R.id.content)).getText().toString();
-            myMqttClient.publish(topic,content);
+           uuid u=new uuid();
+           String s= u.getuuid(this);
+           Toast.makeText(this,s,Toast.LENGTH_SHORT).show();
+
         });
 
     }
