f32de3c7bb60d7f4a37572dd1049ba79443a0430
zilong <angeredream@126.com>
2026-07-01 12:45:34 +0800

    嘟宝初期demo4

--- app/src/main/java/com/zilong/dubao/MainActivity.java
+++ app/src/main/java/com/zilong/dubao/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int aa=1005;
+        int aa=1006;
         if (aa==100){
             Intent intent = new Intent(this, DuMaActivity.class);
             startActivity(intent);
